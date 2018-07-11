79f5f772d13f5d0adfd55872d52b820c987c9e80
Duimy\Silence <597471205@qq.com>
2018-07-11 17:09:33 +0800

    BaseSupportFragment

---
+++ App/src/main/java/com/silence/festec/ui/fragment/TestFragment.kt
@@ -0,0 +1,28 @@
+package com.silence.festec.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.silence.festec.R
+import com.silence.latte.delegates.BaseFragment
+import com.silence.latte.delegates.BaseSupportFragment
+
+/**
+ *
+ *  @author:  PJ
+ *  @time:    2018/7/10 / 15:23
+ *
+ */
+class TestFragment: BaseFragment() {
+
+    override fun setLayout(): Any {
+        return R.layout.fragment_test
+    }
+
+    override fun initView(view: View, savedInstanceState: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
